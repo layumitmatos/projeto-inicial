1342706863ec1f30e0667b4f146eafea1844bd55
layumitmatos <layumi.matos@gmail.com>
2024-03-15 08:24:08 -0300

    variavel nome

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 var botao = document.getElementById("botao"); 
 botao.addEventListener("click", alertar, false); 
+var nome = document.getElementById("nome");
+
 
 function alertar(event){
     alert("Você clicou no botão!" + event); 
-}
 
+
+}
+function alertar(event){
+    alert("Você clicou no botão,"+"" +  nome.value)
+}
