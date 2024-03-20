1cb2af822b9691f8ec5d0dca27c24457d2e68684
layumitmatos <layumi.matos@gmail.com>
2024-03-20 09:52:10 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 var botao = document.getElementById("botao"); 
 botao.addEventListener("click", alertar, false); 
 var nome = document.getElementById("nome");
+var email= document.getElementById("email");
+var saida = document.getElementById("saida"); 
+var telefone = document.getElementById("telefone"); 
+var cep = document.getElementById("cep"); 
+var log = document.getElementById("log"); 
+var numero = document.getElementById("numero"); 
+var comp = document.getElementById("comp"); 
+var bairro = document.getElementById("bairro"); 
+var cidade = document.getElementById("cidade"); 
+var estado =  document.getElementById("estado"); 
 
 
 function alertar(event){
@@ -8,6 +18,36 @@ function alertar(event){
 
 
 }
-function alertar(event){
-    alert("Você clicou no botão,"+"" +  nome.value)
-}
+// function alertar(event){
+//     alert("Você clicou no botão,"+"" +  nome.value)
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Email: " +email.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+var
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
+
+saida.innerText = "Nome: " +nome.value +
+"\n Email:" +email.value; 
